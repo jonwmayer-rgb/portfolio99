84f5ab6ec9b9e37ae3ad416457e3fa9153b48d29
jonwmayer-rgb <jon.w.mayer@gmail.com>
2026-09-10 22:50:30 -0400

    resume

--- .next/server/webpack-runtime.js
+++ .next/server/webpack-runtime.js
@@ -47,11 +47,6 @@
 /******/ 	__webpack_require__.m = __webpack_modules__;
 /******/ 	
 /************************************************************************/
-/******/ 	/* webpack/runtime/amd options */
-/******/ 	(() => {
-/******/ 		__webpack_require__.amdO = {};
-/******/ 	})();
-/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
@@ -130,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("78f5d24e69b9eb99")
+/******/ 		__webpack_require__.h = () => ("55b646cccd25c6c4")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- app/page.tsx
+++ app/page.tsx
@@ -85,7 +85,7 @@ export default function HomePage() {
               Selected Work
             </Link>
             <a
-              href="/resume.pdf"
+              href="/resume.pdf?v=2"
               target="_blank"
               className="group flex items-center gap-1.5 text-stone-600 hover:text-rose-700 transition-colors font-mono text-xs"
             >
